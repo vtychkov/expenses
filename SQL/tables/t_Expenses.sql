2ff36cfc5a11d714d14399c6e5a3cf45042dd6fc
vtychkov <viktychkov@gmail.com>
2018-03-02 21:43:06 +0300

    JW ключи Expenses

--- SQL/tables/t_Expenses.sql
+++ SQL/tables/t_Expenses.sql
@@ -5,5 +5,10 @@ nUserId int NOT NULL,
 nCategorieId smallint NOT NULL,
 nPlaceId smallint NOT NULL,
 nPaymentTypeId tinyint NOT NULL,
-dSum decimal (24,4) NOT NULL
+dSum decimal (24,4) NOT NULL,
+CONSTRAINT PK_t_Expenses PRIMARY KEY(nId),
+CONSTRAINT FK_t_Expenses_t_UsersId FOREIGN KEY (nUserId) REFERENCES t_Users(nId),
+CONSTRAINT FK_t_Expenses_t_PlacesId FOREIGN KEY(nPlaceId ) REFERENCES t_Places(nId),
+CONSTRAINT FK_t_Expenses_t_PaymentTypesId FOREIGN KEY(nPaymentTypeId ) REFERENCES t_PaymentTypes(nId),
+CONSTRAINT FK_t_Expenses_t_CategoriesId FOREIGN KEY(nCategorieId ) REFERENCES t_Categories(nId)
 )
