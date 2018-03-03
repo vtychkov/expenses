dd495bdfc8380b5232c7f3d8a2761bb817cdbf41
vtychkov <viktychkov@gmail.com>
2018-03-03 21:13:22 +0300

    рефакторинг

--- SQL/data/t_dicPaymentTypes_data.sql
+++ SQL/data/t_dicPaymentTypes_data.sql
@@ -1,5 +1,10 @@
-﻿insert t_PaymentTypes
-(sName) values
-(N'наличные'),
-(N'безналичные'),
-(N'электронные')
+﻿set identity_insert dbo.t_dicPaymentTypes on;
+insert into dbo.t_dicPaymentTypes(
+	nId
+	,sName
+)
+values
+	(1, N'наличные'),
+	(2, N'безналичные');
+
+set identity_insert dbo.t_dicPaymentTypes off;
--- SQL/data/t_dicPlaces_data.sql
+++ SQL/data/t_dicPlaces_data.sql
@@ -1,6 +1,12 @@
-﻿insert t_Places (sName,sAdress)
+﻿set identity_insert dbo.t_dicPlaces on;
+insert into dbo.t_dicPlaces(
+	nId
+	,sName
+	,sAdress
+)
 values 
-(N'Глобус', N'дядково'),
-(N'Лента', N'тимуровцев'),
-(N'Шайба', N'есенина'),
-(N'Апельсин', N'есенина')
+	(1, N'Глобус', N'дядково'),
+	(2, N'Лента', N'тимуровцев'),
+	(3, N'Шайба', N'есенина'),
+	(4, N'Апельсин', N'есенина');
+set identity_insert dbo.t_dicPlaces off;
