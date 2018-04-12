3f3dbf9a743de443cd55790395235404fd09ec59
vtychkov <viktychkov@gmail.com>
2018-04-12 19:35:16 +0300

    JW procedure -1

--- SQL/procedure/migration.usp_ExpensesCopy.sql
+++ SQL/procedure/migration.usp_ExpensesCopy.sql
@@ -5,11 +5,12 @@ begin
 	select dtDate, nUserId, nCategoryId, nPlaceId, nPaymentTypeId, dSum
 		from [dbo].[t_Expenses] d
 	where d.nId not in (select m.nId from [migration].[t_Expenses] m)
-	and d.nCategoryId is not null
+	and d.nCategoryId in (select d.nId from [dbo].[t_dicCategories] d)
 	insert migration.t_Expenses(dtDate, nUserId, nCategoryId, nPlaceId, nPaymentTypeId, dSum) 
-	select dtDate, nUserId, 0, nPlaceId, nPaymentTypeId, dSum
+	select dtDate, nUserId, -1, nPlaceId, nPaymentTypeId, dSum
 		from [dbo].[t_Expenses] d
 	where d.nId not in (select m.nId from [migration].[t_Expenses] m)
-	and d.nCategoryId is null
+	and (d.nCategoryId not in (select d.nId from [dbo].[t_dicCategories] d)
+	or d.nCategoryId is null)
 end
 
